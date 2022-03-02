fc25437a5ee83821efca0999fbca3f909d97f86e
孙铤超 <2829908231@qq.com>
2022-03-02 22:25:44 +0800

    rule添加和修改函数的修改

--- internal/dao/rule_dao.go
+++ internal/dao/rule_dao.go
@@ -4,6 +4,7 @@ import (
 	"alert/internal/db"
 	"alert/internal/model"
 	"gorm.io/gorm"
+	"time"
 )
 
 type RuleDao struct {
@@ -15,6 +16,12 @@ func NewRuleDao() *RuleDao {
 }
 
 func (dao *RuleDao) AddRule(rule *model.Rule) (int64, error) {
+	var temp int64
+	if dao.db.Unscoped().Where("code=?", rule.Code).Count(&temp); temp > 0 {
+		result := dao.db.Omit("id").Save(rule).
+			Updates(map[string]interface{}{"created_at": time.Now(), "updated_at": time.Now(), "deleted_at": nil})
+		return result.RowsAffected, result.Error
+	}
 	result := dao.db.Create(&rule)
 	return result.RowsAffected, result.Error
 }
@@ -25,7 +32,7 @@ func (dao *RuleDao) DeleteRuleByID(ID uint) (int64, error) {
 }
 
 func (dao *RuleDao) UpdateRule(rule *model.Rule) (int64, error) {
-	result := dao.db.Save(&rule)
+	result := dao.db.Omit("id").Omit("created_at").Save(&rule).Update("updated_at", time.Now())
 	return result.RowsAffected, result.Error
 }
 
@@ -41,8 +48,8 @@ func (dao *RuleDao) SelectRuleByCode(code string) (*model.Rule, error) {
 	return &rule, result.Error
 }
 
-func (dao *RuleDao) SelectAllRules() (*[]model.Index, error) {
-	var rules []model.Index
+func (dao *RuleDao) SelectAllRules() (*[]model.Rule, error) {
+	var rules []model.Rule
 	result := dao.db.Find(&rules)
 	return &rules, result.Error
 }
--- internal/service/rule_service.go
+++ internal/service/rule_service.go
@@ -1,11 +1,37 @@
 package service
 
-import "alert/internal/dao"
+import (
+	"alert/internal/dao"
+	"alert/internal/model"
+)
 
 type RuleService struct {
-	alertDao dao.RuleDao
+	ruleDao dao.RuleDao
 }
 
 func NewRuleService() *RuleService {
-	return &RuleService{alertDao: *dao.NewRuleDao()}
+	return &RuleService{ruleDao: *dao.NewRuleDao()}
+}
+
+func (service *RuleService) SelectRule(code string) (*model.Rule, error) {
+	return service.ruleDao.SelectRuleByCode(code)
+}
+
+func (service *RuleService) SelectAllRules() (*[]model.Rule, error) {
+	return service.ruleDao.SelectAllRules()
+}
+
+func (service *RuleService) AddRule(roomId uint, name string, code string, ruleType bool, content string) (*model.Rule, error) {
+	var nowType model.RuleType
+	if ruleType {
+		nowType = model.Normal_Rule
+	} else {
+		nowType = model.Complex_Rule
+	}
+	rule := model.Rule{Code: code, Name: name, RoomId: roomId, Type: nowType, Expr: content}
+	_, err := service.ruleDao.AddRule(&rule)
+	if err != nil {
+		return &rule, err
+	}
+	return service.ruleDao.SelectRuleByCode(code)
 }
